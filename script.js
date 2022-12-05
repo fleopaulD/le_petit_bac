1b1f53fc62be975943b156e9039cb6fa56595bab
fleopaulD <flo_thp@netc.pl>
2022-12-06 00:02:14 +0100

    first commit

--- script.js
+++ script.js
@@ -1,35 +1,28 @@
 let letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-var intervalID = null
-
 const mainDiv = document.querySelector(".main")
 
+const buttonDiv = document.createElement("div")
 const startButton = document.createElement("button")
 startButton.classList.add("custom-btn", "btn-11")
 startButton.innerHTML = "Let's gooooo"
-mainDiv.appendChild(startButton);
+buttonDiv.appendChild(startButton);
 
-const remainingLetters = document.createElement("p")
-remainingLetters.innerHTML = `Encore ${letters.length} lettres en stock`
-mainDiv.appendChild(remainingLetters)
+const remainingLettersDiv = document.createElement("p")
+remainingLettersDiv.innerHTML = `Encore ${letters.length} lettres en stock`
+remainingLettersDiv.style.textAlign = "center"
+buttonDiv.appendChild(remainingLettersDiv)
+mainDiv.appendChild(buttonDiv)
 
+const audioAlert = new Audio('alert.mp3')
 
-const letter = document.createElement("div")
-letter.id = ("letter")
-
-const timer = document.createElement("div")
-timer.id = ("timer")
-timer.innerText = "--:--"
-
-var audioAlert = new Audio('alert.mp3')
-
-async function generateAnimLetter(n) {
+async function generateLettersAnim(n) {
     if(n === 0) return
 
     const letter = letters[Math.floor(Math.random() * letters.length)][0]
     await wait(80)
     document.querySelector("#letter").innerText = letter.toUpperCase()
-    await generateAnimLetter(n - 1)
+    await generateLettersAnim(n - 1)
 }
 
 function wait (duration) {
@@ -56,12 +49,18 @@ async function timerDisplay(duration) {
 }
 
 startButton.addEventListener("click", async function() {
-    mainDiv.removeChild(startButton)
-    mainDiv.removeChild(remainingLetters)
+    const timer = document.createElement("div")
+    timer.id = ("timer")
+    timer.innerText = "--:--"
+
+    const letter = document.createElement("div")
+    letter.id = ("letter")
+    
+    mainDiv.removeChild(buttonDiv)
     mainDiv.appendChild(letter)
     mainDiv.appendChild(timer)
 
-    await generateAnimLetter(30)
+    await generateLettersAnim(30)
 
     const choosenLetter = letters.splice([Math.floor(Math.random() * letters.length)], 1)[0]
     document.querySelector("#letter").innerText = choosenLetter.toUpperCase()
@@ -70,7 +69,6 @@ startButton.addEventListener("click", async function() {
     audioAlert.play()
     mainDiv.removeChild(letter)
     mainDiv.removeChild(timer)
-    mainDiv.appendChild(startButton)
-    remainingLetters.innerHTML = `Encore ${letters.length} lettres en stock`
-    mainDiv.appendChild(remainingLetters)
+    remainingLettersDiv.innerHTML = `Encore ${letters.length} lettres en stock`
+    mainDiv.appendChild(buttonDiv)
 })
